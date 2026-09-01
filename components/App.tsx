9e72a7ca743b5b4b421511017611ad79a13a3ba2
sneakersspicy03-svg <admin@spicy.com>
2026-09-01 17:40:54 -0400

    fix(version): bump internal versionCode to 6 (v2.2.1) to dismiss updater modal

--- components/App.tsx
+++ components/App.tsx
@@ -20,8 +20,8 @@ import DeveloperMode from './DeveloperMode';
 import AIConsultant from './AIConsultant';
 import AppUpdateModal, { AppUpdateInfo } from './AppUpdateModal';
 
-export const CURRENT_APP_VERSION = "2.2.0";
-export const CURRENT_VERSION_CODE = 5;
+export const CURRENT_APP_VERSION = "2.2.1";
+export const CURRENT_VERSION_CODE = 6;
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
